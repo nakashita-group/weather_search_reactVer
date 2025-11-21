9194440f0775bfdceef65e1a8028b8e681345fa3
sdp202401 <sdp202401@RPA212>
2025-11-21 13:59:25 +0900

    認証

--- src/App.jsx
+++ src/App.jsx
@@ -8,40 +8,40 @@ function App() {
   const [inquireText, setInquireText] = useState("");
   const [UserName, setUserName] = useState("");
 
-//   useEffect(()=>{
-//     if (!window.woff) return;
+  useEffect(()=>{
+    if (!window.woff) return;
 
-//     window.woff
-//   .init({
-//     //EndPointURLに初めてリダイレクトされるタイミングで実行
-//     woffId: "aVitSpcsneyMoOxvh0_Lug"
-//   })
-//   .then(() => {
-//     console.log("初期化完了！！");
+    window.woff
+  .init({
+    //EndPointURLに初めてリダイレクトされるタイミングで実行
+    woffId: "aVitSpcsneyMoOxvh0_Lug"
+  })
+  .then(() => {
+    console.log("初期化完了！！");
 
-//     //ログイン情報がない場合ログイン画面表示
-//     if (!window.woff.getAccessToken()) {
-//       console.log("トークンなし → 認可フロー開始");
-//       window.woff.login();
-//       return;
-//     }
+    //ログイン情報がない場合ログイン画面表示
+    if (!window.woff.getAccessToken()) {
+      console.log("トークンなし → 認可フロー開始");
+      window.woff.login();
+      return;
+    }
 
-//     console.log(window.woff.getAccessToken());
+    console.log(window.woff.getAccessToken());
 
-//     //プロフィール情報取得
-//     window.woff.getProfile()
-//       .then((profile) => {
-//         console.log("プロフィール情報:", profile);
-//         setUserName(profile.displayName);
-//       })
+    //プロフィール情報取得
+    window.woff.getProfile()
+      .then((profile) => {
+        console.log("プロフィール情報:", profile);
+        setUserName(profile.displayName);
+      })
 
-//   })
-//   .catch((err) => {
-//     // 初期化処理中にエラーが発生した場合
-//     console.log("エラー発生！！");
-//     console.log(err.code, err.message);
-//   });
-//   },[]);
+  })
+  .catch((err) => {
+    // 初期化処理中にエラーが発生した場合
+    console.log("エラー発生！！");
+    console.log(err.code, err.message);
+  });
+  },[]);
 
   const OnNameChange = (e) => {
     setNameText(e.target.value);
